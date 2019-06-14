b9154fe7ccc77d95b05018b040cb7df13604c07a
tahsinelahinavin <49580504+tahsinelahinavin@users.noreply.github.com>
2019-06-14 11:36:08 -0400

    Update list_vs_yield.py

--- list_vs_yield.py
+++ list_vs_yield.py
@@ -18,6 +18,9 @@ def simpleGenerator(n):
 
 def lazyGenerator(n):
     i=1
+# Return sends a specified value back to its caller whereas Yield can produce a sequence of values.
+# We should use yield when we want to iterate over a sequence, 
+# but don’t want to store the entire sequence in memory.
     while i<=n:
         yield cube(i)
         i+=1
